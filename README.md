d235cea768bd47eb4edb9a93ecf59bcc838c40eb
anonymous0010 <ctoxtli@gmail.com>
2017-05-02 21:30:04 -0400

    New changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# deep-learning-tweets-text-classifier-word2vec
+# deep-learning-tweets-classifier
 
-This tool uses Word2Vec and Bag of Words combined with Neural Networks, SVN, KNN, Naive Bayes, Decision Trees and ExtraTrees to classify tweets according to an annotated file. This classifier was used to classify tweets from Twitter.
+This tool uses Word2Vec and Bag of Words combined with Neural Networks, SVN, KNN, Naive Bayes, Decision Trees and ExtraTrees to classify tweets according to an annotated file.
 
 Run this in command line:
 
@@ -10,16 +10,10 @@ Run this in command line:
 
 > sudo pip install -r requirements.txt
 
-Demo files are in place in order to have a clear idea about how this works.
+Place your data.csv in the directory. It only requires the column "Text" where the tweets text is placed and the column "Class" than contains the annotated categories.
 
 Run the python script:
 
 > python app.py
 
-or
-
-> python app.py -t train.csv -i predict.csv -o results.csv -w glove.6B.50d.txt
-
-The tool will print the results in the terminal and store in the results.csv file.
-
-This classifier is multilingual, for instance if you want to run it in spanish you can use this word vector file http://cs.famaf.unc.edu.ar/~ccardellino/SBWCE/SBW-vectors-300-min5.txt.bz2 instead of the glove file.
+The tool will print the results in the terminal.
--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import warnings
+def warn(*args, **kwargs):
+    pass
+warnings.warn = warn
+import sys
 import gensim
+import getopt
 import pandas
 import numpy as np
 from scipy import interp
@@ -8,16 +14,18 @@ from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation, metrics, svm
 from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, AdaBoostClassifier
 
+TRAIN_FILE = "train.csv"
+INPUT_FILE = "predict.csv"
+OUTPUT_FILE = "results.csv"
+W2V_FILE = "glove.6B.50d.txt"
 FOLDS = 10
-W2V_FILE = "glove.6B.300d.txt"
 
 class MeanEmbeddingVectorizer(object):
     def __init__(self, word2vec):
@@ -64,6 +72,11 @@ def load_data_and_labels(data_path):
     labels = data_file.Class.tolist()
     return x_text,labels
 
+def load_data(data_path):
+    data_file = pandas.read_csv(data_path)
+    x_text = data_file.Text.tolist()
+    return [s.strip() for s in x_text], [s.strip().split() for s in x_text]
+
 def preprocess_w2v(X, y):
     X = np.array(X)
     y = np.array(y)
@@ -76,7 +89,6 @@ def preprocess_w2v(X, y):
         X_test.append(X_test_value)
         y_train.append(y_train_value)
         y_test.append(y_test_value)
-    #X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y)
     return X_train, X_test, y_train, y_test
 
 def preprocess_bag(X, y):
@@ -116,13 +128,7 @@ def train(clf, X_train, X_test, y_train, y_test):
         fpr, tpr, thresholds = metrics.roc_curve(y_test[fl], pred)
         roc_auc = metrics.auc(fpr, tpr)
         mean_tpr += interp(mean_fpr, fpr, tpr)
-        # print("Fold "+str(fl))
         print("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (fl, precision, recall, f1, support, acc, conf_matrix[0][0], conf_matrix[0][1], conf_matrix[1][0], conf_matrix[1][1]))
-        # print(metrics.classification_report(y_test[fl], pred))
-        # print("Acc:"+str(acc))
-        # print(metrics.confusion_matrix(y_test[fl], pred))
-        # mean_tpr[0] = 0.0
-        # plt.plot(fpr, tpr, lw=lw, label='ROC fold %d (area = %0.2f)' % (fl, roc_auc))
         acc_tot += acc
         precision_tot += precision
         recall_tot += recall
@@ -158,8 +164,109 @@ def plot(title, records):
     plt.legend(loc="lower right")
     plt.show()
 
+def benchmark(input_file):
+    X, y = load_data_and_labels(input_file)
+    clfs = [ExtraTreesClassifier(n_estimators=200),
+            svm.SVC(C=1, kernel='linear'),
+            KNeighborsClassifier(n_neighbors=3),
+            DecisionTreeClassifier(random_state=0),
+            MLPClassifier(alpha=1),
+            GaussianNB()]
+    names = ['ExtraTrees', 'SVM', 'KNN', 'DecisionTree', 'NeuralNetwork', 'NaiveBayes']
+    print("WORD2VEC")
+    with open(W2V_FILE, "rb") as lines:
+        w2v = {line.split()[0]: np.array(map(float, line.split()[1:])) for line in lines}
+    X_train, X_test, y_train, y_test = preprocess_w2v(X,y)
+    cont = 0
+    sections = ['Word2Vec', 'BagOfWords']
+    results = {}
+    section = sections[cont]
+    print(section)
+    results[section] = {}
+    for i in range(len(names)):
+        print(section + " " + names[i])
+        clf = Pipeline([
+            (section + " vectorizer", MeanEmbeddingVectorizer(w2v)),
+            ("classifier", clfs[i])])
+        results[section][names[i]] = train(clf, X_train, X_test, y_train, y_test)
+    cont += 1
+    section = sections[cont]
+    print(section)
+    results[section] = {}
+    X_train, X_test, y_train, y_test = preprocess_bag(X,y)
+    for i in range(len(names)):
+        print(section + " " + names[i])
+        clf = clfs[i]
+        results[section][names[i]] = train(clf, X_train, X_test, y_train, y_test)
+    for name in names:
+        records = []
+        for section in sections:
+            values = results[section][name]
+            values['label'] = section
+            records.append(values)
+        plot('Comparison using ' + name, records)
 
-X, y = load_data_and_labels("data.csv")
+def predict(params):
+    clf = ExtraTreesClassifier(n_estimators=200)
+    with open(params["w2v_file"], "rb") as lines:
+        w2v = {line.split()[0]: np.array(map(float, line.split()[1:])) for line in lines}
+    clf_w2v = Pipeline([
+        ("word2vec vectorizer", TfidfEmbeddingVectorizer(w2v)),
+        ("classifier", clf)])
+    X, y = load_data_and_labels(params["train_file"])
+    col_y, arr_X, arr_y = {}, [], []
+    for i in range(len(y)):
+        if y[i] not in col_y:
+            col_y[y[i]] = []
+        col_y[y[i]].append(X[i])
+    for field in col_y:
+        arr_X.append(col_y[field])
+        arr_y.append(field)
+    clf_w2v.fit(arr_X, arr_y)
+    raw_data = {}
+    raw_data['Text'], tokenized = load_data(params["input_file"])
+    raw_data['Class'] = clf_w2v.predict(tokenized)
+    data_frame = pandas.DataFrame(raw_data)
+    data_frame.to_csv(params["output_file"])
+    print(data_frame)
+    return raw_data
+
+def run(params):
+    if params["bench"]:
+        benchmark(params)
+    else:
+        predict(params)
+
+def main(argv):
+    params = {
+        "bench": False,
+        "train_file": TRAIN_FILE,
+        "input_file": INPUT_FILE,
+        "output_file": OUTPUT_FILE,
+        "w2v_file": W2V_FILE
+    }
+    opts, args = getopt.getopt(argv, "t:i:o:w:b")
+    if opts:
+        for o, a in opts:
+            if o in ("-b"):
+                params["bench"] = True
+            elif o in ("-t"):
+                params["train_file"] = a
+            elif o in ("-i"):
+                params["input_file"] = a
+            elif o in ("-o"):
+                params["output_file"] = a
+            elif o in ("-w"):
+                params["w2v_file"] = a
+    run(params)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
+# page
+# http://nadbordrozd.github.io/blog/2016/05/20/text-classification-with-word2vec/
+
 """
 clfs = [ExtraTreesClassifier(n_estimators=200),
         svm.SVC(C=1, kernel='linear'),
@@ -169,50 +276,8 @@ clfs = [ExtraTreesClassifier(n_estimators=200),
         GaussianNB()]
 names = ['ExtraTrees', 'SVM', 'KNN', 'DecisionTree', 'NeuralNetwork', 'NaiveBayes']
 """
-clfs = [ExtraTreesClassifier(n_estimators=200),
-        svm.SVC(C=1, kernel='linear'),
-        KNeighborsClassifier(n_neighbors=3),
-        DecisionTreeClassifier(random_state=0),
-        MLPClassifier(alpha=1),
-        GaussianNB()]
-names = ['ExtraTrees', 'SVM', 'KNN', 'DecisionTree', 'NeuralNetwork', 'NaiveBayes']
 
-print("WORD2VEC")
-with open(W2V_FILE, "rb") as lines:
-    w2v = {line.split()[0]: np.array(map(float, line.split()[1:])) for line in lines}
-X_train, X_test, y_train, y_test = preprocess_w2v(X,y)
-
-cont = 0
-sections = ['Word2Vec', 'BagOfWords']
-
-results = {}
-section = sections[cont]
-print(section)
-results[section] = {}
-for i in range(len(names)):
-    print(section + " " + names[i])
-    clf = Pipeline([
-        (section + " vectorizer", MeanEmbeddingVectorizer(w2v)),
-        ("classifier", clfs[i])])
-    results[section][names[i]] = train(clf, X_train, X_test, y_train, y_test)
-
-cont += 1
-section = sections[cont]
-print(section)
-results[section] = {}
-X_train, X_test, y_train, y_test = preprocess_bag(X,y)
-for i in range(len(names)):
-    print(section + " " + names[i])
-    clf = clfs[i]
-    results[section][names[i]] = train(clf, X_train, X_test, y_train, y_test)
-
-for name in names:
-    records = []
-    for section in sections:
-        values = results[section][name]
-        values['label'] = section
-        records.append(values)
-    plot('Comparison using ' + name, records)
+#X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y)
 
 """
 etree_w2v_tfidf = Pipeline([
@@ -247,3 +312,12 @@ train(etree_w2v_tfidf, X_train, X_test, y_train, y_test)
 # acc_tfidf = metrics.accuracy_score(y_test,etree_w2v_tfidf.predict(X_test))
 # print("model acc:"+str(acc))
 # print("model acc_tfidf:"+str(acc_tfidf))
+
+# print(metrics.classification_report(y_test[fl], pred))
+# print("Acc:"+str(acc))
+# print(metrics.confusion_matrix(y_test[fl], pred))
+# mean_tpr[0] = 0.0
+# plt.plot(fpr, tpr, lw=lw, label='ROC fold %d (area = %0.2f)' % (fl, roc_auc))
+
+# print("Fold "+str(fl))
+
